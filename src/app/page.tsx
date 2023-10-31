303c03c81ead4da9d7a3ae46c344b02a3fbf336b
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2023-10-31 20:52:57 +0100

    ready for push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,80 +1,67 @@
+"use client"
 import Image from "next/image"
 import styles from "./page.module.css"
+import { Container } from "@/components/Container"
 
 export default function Home() {
   return (
     <>
-      <header class="BoxShadow">
-        <div class="Logo">
-          <h1>LUKAS LÅÅS</h1>
-        </div>
-        <nav>
-          <a class="navItems" href="#workExperience">
-            EXPERIENCE
-          </a>
-          <a class="navItems" href="#education">
-            EDUCATION
-          </a>
-          <a class="navItems" href="#projects">
-            PROJECTS
-          </a>
-          <a class="navItems" href="#programmingLanguages">
-            PROGRAMMING LANGUAGES
-          </a>
-          <a class="navItems" href="#languages">
-            LANGUAGES
-          </a>
-          <a class="navItems" href="#otherMerits">
-            MERITS
-          </a>
-          <a class="navItems" href="#contact">
-            CONTACT
-          </a>
-        </nav>
-      </header>
       <main className={styles.main}>
-        <div class="Background"></div>
+        <div className="Background"></div>
+        <Container
+          header={<>WORK EXPERIENCE</>}
+          content={[
+            {
+              sub_header: (
+                <>
+                  {" "}
+                  2 MONTH SUMMER JOB AT{" "}
+                  <a href="https://brick.tech" style={{ fontSize: "inherit" }}>
+                    BRICK TECHNOLOGY
+                  </a>
+                </>
+              ),
+              body: (
+                <>
+                  <p>
+                    I worked on their admin dashboard as a full-stack developer.
+                    The dashboard is written in React using TypeScript which
+                    fetches data from DynamoDB and the endpoints are written in
+                    TypeScript. One of the things I added was a feature to edit
+                    the pictures of brick venues from their dashboard.
+                  </p>
+                  <p>
+                    Added some features to their public website, including some
+                    translations and adding some new content.
+                  </p>
+                </>
+              ),
+            },
+            {
+              sub_header: (
+                <>
+                  10 WEEK INTERNSHIP AT
+                  <a href="https://brick.tech" style={{ fontSize: "inherit" }}>
+                    BRICK TECHNOLOGY
+                  </a>
+                </>
+              ),
+              body: (
+                <>
+                  <p>
+                    As part of my education I had a 10 week internship, during
+                    this internship I worked on their admin dashboard as a
+                    front-end developer. The page is written in react using
+                    TypeScript.
+                  </p>
+                </>
+              ),
+            },
+          ]}
+        />
 
-        <div id="workExperience" class="Container BoxShadow">
-          <div class="title">
-            <h1>WORK EXPERIENCE</h1>
-          </div>
-          <div class="divider">
-            <h2>
-              2 MONTH SUMMER JOB AT{" "}
-              <a href="https://brick.tech" style={{ fontSize: "inherit" }}>
-                BRICK TECHNOLOGY
-              </a>
-            </h2>
-            <p>
-              I worked on their admin dashboard as a full-stack developer. The
-              dashboard is written in React using TypeScript which fetches data
-              from DynamoDB and the endpoints are written in TypeScript. One of
-              the things I added was a feature to edit the pictures of brick
-              venues from their dashboard.
-            </p>
-            <p>
-              Added some features to their public website, including some
-              translations and adding some new content.
-            </p>
-            <p></p>
-          </div>
-          <div>
-            <h2>
-              10 WEEK INTERNSHIP AT
-              <a href="https://brick.tech" /* style="font-size: inherit" */>
-                BRICK TECHNOLOGY
-              </a>
-            </h2>
-            <p>
-              As part of my education I had a 10 week internship, during this
-              internship I worked on their admin dashboard as a front-end
-              developer. The page is written in react using TypeScript.
-            </p>
-          </div>
-        </div>
-        <div id="education" class="Container BoxShadow">
-          <div class="title">
+        <div id="education" className="Container BoxShadow">
+          <div className="title">
             <h1>EDUCATION</h1>
           </div>
           <div>
@@ -96,32 +83,29 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div id="projects" class="Container BoxShadow">
-          <div class="title">
+        <div id="projects" className="Container BoxShadow">
+          <div className="title">
             <h1>SCHOOL PROJECTS</h1>
           </div>
-          <div class="divider">
+          <div className="divider">
             <h2>nti-nedräkning</h2>
             <p>
               Website designed to display the time left in different courses and
               total time left until summer, written in next using typescript.
             </p>
             <a href="https://nti-nedrakning.vercel.app" target="_blank">
-              <img
-                /* style="width: 100%" */
-                src="assets/images/nti-nedrakning.PNG"
+              <Image
+                style={{ width: "50%", height: "auto" }}
+                src="/images/nti-nedrakning.PNG"
                 alt="Screenshot of website"
+                width={828}
+                height={465}
               />
-            </a>
-            <a
-              class="linkStyle"
-              href="https://nti-nedrakning.vercel.app"
-              target="_blank"
-            >
+              <br />
               nti-nedrakning.vercel.app
             </a>
           </div>
-          <div class="divider">
+          <div className="divider">
             <h2>Lunch-pad</h2>
             <p>
               Too keep track of how many students eat every day we built a
@@ -129,13 +113,15 @@ export default function Home() {
               added to a counter and by the end of each day the data is uploaded
               to a google spreadsheet.
             </p>
-            <img
-              /*             style="width: 100%"
-               */ src="assets/images/lunchpad.png"
+            <Image
+              style={{ width: "50%", height: "auto" }}
+              src="/images/lunchpad.png"
               alt="Picture of lunchpad"
+              width={1920}
+              height={1080}
             />
           </div>
-          <div class="divider">
+          <div className="divider">
             <h2>Pizzeria Rafiki</h2>
             <p>
               Website designed for a mock pizzeria. The website's functions are
@@ -145,17 +131,14 @@ export default function Home() {
               href="https://ntig-uppsala.github.io/pizzeria-rafiki/"
               target="_blank"
             >
-              <img
-                /*               style="width: 100%"
-                 */ src="assets/images/pizzeriarafiki.PNG"
+              <Image
+                style={{ width: "50%", height: "auto" }}
+                src="/images/pizzeriarafiki.PNG"
                 alt="Screenshot of website"
+                width={1920}
+                height={1080}
               />
-            </a>
-            <a
-              class="linkStyle"
-              href="https://ntig-uppsala.github.io/pizzeria-rafiki/"
-              target="_blank"
-            >
+              <br />
               ntig-uppsala.github.io/pizzeria-rafiki/
             </a>
           </div>
@@ -169,23 +152,20 @@ export default function Home() {
               href="https://ntig-uppsala.github.io/Bengans-Biluthyrning/"
               target="_blank"
             >
-              <img
-                /*               style="width: 100%"
-                 */ src="assets/images/bengansbiluthyrning.PNG"
+              <Image
+                style={{ width: "50%", height: "auto" }}
+                src="/images/bengansbiluthyrning.PNG"
                 alt="Screenshot of website"
+                width={1920}
+                height={1080}
               />
-            </a>
-            <a
-              class="linkStyle"
-              href="https://ntig-uppsala.github.io/Bengans-Biluthyrning/"
-              target="_blank"
-            >
+              <br />
               ntig-uppsala.github.io/Bengans-Biluthyrning/
             </a>
           </div>
         </div>
-        <div id="programmingLanguages" class="Container BoxShadow">
-          <div class="title">
+        <div id="programmingLanguages" className="Container BoxShadow">
+          <div className="title">
             <h1>PROGRAMMING LANGUAGES</h1>
           </div>
           <p>Python</p>
@@ -194,20 +174,25 @@ export default function Home() {
           <p>TypeScript (React, NEXT)</p>
           <p>C#</p>
         </div>
-        <div id="languages" class="Container BoxShadow">
-          <div class="title">
+        <div id="languages" className="Container BoxShadow">
+          <div className="title">
             <h1>LANGUAGES</h1>
           </div>
           <p>Swedish - first language</p>
           <p>English - fluent</p>
         </div>
-        <div id="otherMerits" class="Container BoxShadow">
-          <div class="title">
+        <div id="otherMerits" className="Container BoxShadow">
+          <div className="title">
             <h1>OTHER MERITS</h1>
           </div>
           <h2>Certificate</h2>
           <p>MTA: Introduction to Programming Using Python</p>
-          <div /* style="background-color: white; width: 150px; height: 270px" */
+          <div
+            style={{
+              backgroundColor: "white",
+              width: "150px",
+              height: "270px",
+            }}
           >
             <div
               data-iframe-width="150"
@@ -223,15 +208,15 @@ export default function Home() {
           </div>
           <h2>GitHub</h2>
           <a
-            class="linkStyle"
+            className="linkStyle"
             href="https://github.com/lukas-laas"
             target="_blank"
           >
             github.com/lukas-laas
           </a>
         </div>
-        <div id="contact" class="Container BoxShadow">
-          <div class="title">
+        <div id="contact" className="Container BoxShadow">
+          <div className="title">
             <h1>CONTACT</h1>
           </div>
           <a href="mailto:lukas.s.laas@gmail.com" id="MailAdress">
---
+++ src/components/Container.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react"
+
+type Props = {
+  header: ReactNode
+  content: item[]
+}
+type item = {
+  sub_header: ReactNode
+  body: ReactNode
+}
+
+export const Container = (Props: Props) => {
+  return (
+    <div id="workExperience" className="Container BoxShadow">
+      <div className="title">
+        <h1>{Props.header}</h1>
+      </div>
+      {Props.content.map((item, i) => (
+        <div key={i} className="divider">
+          <h2>{item.sub_header}</h2>
+          <p>{item.body}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
