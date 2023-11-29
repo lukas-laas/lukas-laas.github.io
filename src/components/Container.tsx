6de7ff2a85b0e0065ea9f7d87bb889f05763985c
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2023-11-29 15:09:37 +0100

    remove broken elements

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,4 @@
 import { ReactNode } from "react"
-import Slider from "@madzadev/image-slider"
-const images = [
-  { url: "/images/image (1).png" },
-  { url: "/images/image (2).png" },
-  { url: "/images/image (3).png" },
-  { url: "/images/image (4).png" },
-  { url: "/images/image (5).png" },
-]
 
 type Props = {
   header: ReactNode
@@ -28,11 +20,6 @@ export const Container = (Props: Props) => {
         <div key={i} className="divider">
           <h2>{item.sub_header}</h2>
           <p>{item.body}</p>
-          {item.imageSlider ? (
-            <>
-              <Slider imageList={images} width={1000} height={600} />
-            </>
-          ) : null}
         </div>
       ))}
     </div>
